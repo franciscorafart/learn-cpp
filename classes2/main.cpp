4cec64f63e48217dc8e516d5dc6259ddc11eaa15
franciscorafart <francisco.rafart@gmail.com>
2023-07-14 08:04:55 -0400

    Add practical use of enums

--- classes2/main.cpp
+++ classes2/main.cpp
@@ -2,34 +2,35 @@
 
 class Log {
 public:
-    const int LogLevelError = 0;
-    const int LogLevelWarning = 1;
-    const int LogLevelInfo = 2;
+    enum Level {
+        LevelError, LevelWarning, LevelInfo
+    };
      
 private:
-    int m_LogLevel = LogLevelInfo; // m_ is convention for class private property
+    // Use Level type instead of int to restrict the possible values, though it's still an int
+    Level m_LogLevel = Level::LevelInfo; // m_ is convention for class private property
 
 public:
-    void SetLevel(int level) {
+    void SetLevel(Level level) {
         m_LogLevel = level;
     }
     void Warn(const char* message){
-        if (m_LogLevel >= LogLevelWarning)
+        if (m_LogLevel >= LevelWarning) // enum is not a namespace in itself, so we can call it without Level::LevelWarning
             std::cout <<  "[Warning] " << message << std::endl;
     }
     void Info(const char* message){
-        if (m_LogLevel >= LogLevelInfo)
+        if (m_LogLevel >= LevelInfo)
             std::cout <<  "[Info] " << message << std::endl;
     }
     void Error(const char* message){
-        if (m_LogLevel >= LogLevelError)
+        if (m_LogLevel >= LevelError)
             std::cout <<  "[Error] " << message << std::endl;
     }
 };
 
 int main(int argc, const char * argv[]) {
     Log log;
-    log.SetLevel(log.LogLevelWarning);
+    log.SetLevel(log.LevelWarning);
     log.Warn("Hello!");
     log.Error("Hello1");
     log.Info("Hello!");
